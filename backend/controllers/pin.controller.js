5538f5a9666412f712a0c873d0b2d4e3f22916fb
karim said <karimsaid214@gmail.com>
2025-10-15 22:09:13 +0300

    search logic

--- backend/controllers/pin.controller.js
+++ backend/controllers/pin.controller.js
@@ -1,19 +1,41 @@
-import pinModel from "../models/pin.model.js"
+import pinModel from "../models/pin.model.js";
 
+export const getPins = async (req, res, next) => {
+  const LIMIT = 21;
+  const pageParam = Number(req.query.cursor) || 0;
+  const search = req.query.search;
 
-export const getPins =async (req, res , next )=>{
+  const pins = await pinModel
+    .find(
+      search
+        ? {
+            $or: [
+              { title: { $regex: search, $options: "i" } },
+              { description: { $regex: search } },
+              { tags: { $in: [search] } },
+            ],
+          }
+        : {}
+    )
+    .limit(LIMIT)
+    .skip(LIMIT * pageParam);
 
-  const pageParam = req.query.cursor*1 || 0
-  const LIMIT= 21
-  const total = await pinModel.countDocuments()
- const pins = await pinModel.find().limit(LIMIT).skip( LIMIT*pageParam )  
-  const hasNextPage = pins.length === LIMIT
+  const hasNextPage = pins.length === LIMIT;
+  setTimeout(() => console.log("wait"), 3000);
 
-  return res.json({pins , nextCursor:hasNextPage ?pageParam+1 : null , total})
+  return res.json({ pins, nextCursor: hasNextPage ? pageParam + 1 : null });
+};
+
+export const  getPin = async(req, res, next)=>{
+  const id = req.params.id
+  const pin = await pinModel.findById(id)
+  res.json(pin?pin:'no such pin exist')
+
 }
 
-export const deletePins = async (req , res, next)=>{
+
+export const deletePins = async (req, res, next) => {
   await pinModel.deleteMany({ index: { $gte: 50, $lte: 100 } });
 
-return res.json('pins deleted successfully')
-}
+  return res.json("pins deleted successfully");
+};
